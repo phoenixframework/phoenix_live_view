3744f3f21e0f9a743a993e3e6468b915c4239e47
Chris McCord <chris@chrismccord.com>
2018-11-12 10:17:23 -0500

    First pass docs overview

--- lib/phoenix_live_view.ex
+++ lib/phoenix_live_view.ex
@@ -11,12 +11,94 @@ defmodule Phoenix.LiveView do
   A `:signing_salt` configuration is required in your endpoint's
   `:live_view` configuration, for example:
 
-      config :my_app, MyAppWeb.Endpoint,
+      config :my_app, AppWeb.Endpoint,
         ...,
         live_view: [signing_salt: ...]
 
   You can generate a secure, random signing salt with the
   `mix phx.gen.secret 32` task.
+
+  ## Life-cycle
+
+  A live view begins as a regular HTTP request and HTML response,
+  and then upgrades to a stateful view on client connect,
+  guaranteeing a regular HTML page even if JavaScript is disabled.
+
+  You begin by rendering a live view from the controller and providing
+  *session* data to the view, which represents request information
+  necessary for the view, such as params, cookie session info, etc.
+  The session is signed and stored on the client, then provided back
+  to the server when the client connects, or reconnects to the stateful
+  view. When a view is rendered from the controller, the `mount/2` callback
+  is invoked with the provided session data and the live view's socket.
+  The `mount/2` callback wires up socket assigns necessary for rendering
+  the view. After mounting, `render/1` is invoked and the HTML is sent
+  as a regular HTML response to the client.
+
+  After rendering the static page with a signed session, the live
+  views connect from the client where stateful views are spawned
+  to push rendered updates to the browser, and receive client events
+  via phx bindings. Just like the controller flow, `mount/2` is invoked
+  with the signed session, and socket state, where mount assigns
+  values for rendering. However, in the connected client case, a
+  live view process is spawned on the server, pushes the result of
+  `render/1` to the client and contiues on for the duration of the
+  connection. If at any point during the stateful life-cycle a
+  crash is encountered, or the client connection drops, the client
+  gracefully reconnects to the server, passing its signed session
+  back to `mount/2`.
+
+  ## Usage
+
+  First, a live view requires two callbacks: `mount/2` and `render/1`:
+
+      def AppWeb.ThermostatView do
+        def render(assigns) do
+          ~E\"""
+          Current temperature: <%= @temperature %>
+          \"""
+        end
+
+        def mount(%{id: id}, socket) do
+          case Thermostat.get_reading(id) do
+            {:ok, temperature} ->
+              {:ok, assign(socket, :temperature, temperature)}
+
+            {:error, reason} ->
+              {:error, reason}
+          end
+        end
+      end
+
+  The `render/1` callback receives the `socket.assigns` and is responsible
+  for returning rendered content. You can use `Phoenix.HTML.sigil_E` to inline
+  Phoenix HTML EEx templates.
+
+  With a live view defined, you first call `live_render` from a controller:
+
+      def show(conn, %{"id" => thermostat_id}) do
+        Phoenix.LiveView.live_render(conn, AppWeb.ThermostatView, session: %{
+          thermostat_id: id,
+          current_user_id: get_session(conn, :user_id),
+        })
+      end
+
+  As we've seen, you pass `:session` data about the request to the view,
+  such as the current user's id in the cookie session, and parameters
+  from the request. A regular HTML response is sent with a signed token
+  embedded in the DOM.
+
+
+  Next, your client code connects to the server:
+
+      import LiveSocket from "live_view"
+
+      let liveSocket = new LiveSocket("/live")
+      liveSocket.connect()
+
+
+
+
   """
 
   @behaviour Plug
@@ -51,20 +133,20 @@ defmodule Phoenix.LiveView do
 
   ## Examples
 
-      def MyAppWeb.ThermostatController do
+      def AppWeb.ThermostatController do
         def show(conn, %{"id" => thermostat_id}) do
-          live_render(conn, MyAppWeb.ThermostatView, session: %{
+          live_render(conn, AppWeb.ThermostatView, session: %{
             thermostat_id: id,
             current_user_id: get_session(conn, :user_id),
           })
         end
       end
 
-      def MyAppWeb.ThermoStatView do
+      def AppWeb.ThermostatView do
         def render(assigns) do
           ~E\"""
           Current temperature: <%= @temperatures %>
-          <%= live_render(conn, MyAppWeb.ClockView) %>
+          <%= live_render(conn, AppWeb.ClockView) %>
           \"""
         end
       end
