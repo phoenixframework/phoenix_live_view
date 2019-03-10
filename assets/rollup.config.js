70af08488880afe6b6417bad4cd81b655ae548f0
snewcomer <snewcomer24@gmail.com>
2019-03-10 14:40:29 -0700

    Alternate rollup implementation

---
+++ assets/rollup.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { terser } from "rollup-plugin-terser";
+import { sizeSnapshot } from "rollup-plugin-size-snapshot";
+
+
+export default {
+  // external: [] // TODO make phoenix.js a global and not bundle in this
+  input: './js/phoenix_live_view.js',
+  output: {
+    file: path.resolve(__dirname, '../priv/static/phoenix_live_view.js'),
+    format: 'es',
+    sourcemap: true
+  },
+  plugins: [
+    resolve(),
+
+    commonjs({
+      namedExports: {
+        '../deps/phoenix/priv/static/phoenix.js': ['Socket']
+      }
+    }),
+
+    babel({
+      exclude: ['node_modules/**','../deps/**'],
+    }),
+
+    (process.env.BUILD === 'production' && terser()),
+
+    sizeSnapshot()
+  ]
+};
+
