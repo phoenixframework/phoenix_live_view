689ea23b88f7c388b996340a3cd0b4379a22c7ad
Chris McCord <chris@chrismccord.com>
2019-03-14 12:06:26 -0400

    Use Phoenix 1.4.2

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Phoenix.LiveView.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:phoenix, github: "phoenixframework/phoenix", branch: "v1.4"},
+      {:phoenix, "~> 1.4.2"},
       {:phoenix_html, "~> 2.13"},
       {:jason, "~> 1.0", optional: true}
     ]
