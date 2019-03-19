3ca678d81163a1f923e560d21e03a24a7d8f0ce7
snewcomer <snewcomer24@gmail.com>
2019-03-19 16:39:43 -0700

    group in describe block

--- assets/__tests__/live_socket_test.js
+++ assets/__tests__/live_socket_test.js
@@ -28,192 +28,194 @@ function dom() {
   return new JSDOM(`<!DOCTYPE html>${liveViewDOM().outerHTML}`);
 }
 
-test('sets defaults', async () => {
-  let opts = { viewLogger: 'foo' };
-  let liveSocket = new LiveSocket('/live', opts);
-  expect(liveSocket.socket).toBeDefined();
-  expect(liveSocket.socket.onOpen).toBeDefined();
-  expect(liveSocket.opts.reconnectAfterMs).toBeDefined();
-  expect(liveSocket.opts.viewLogger).toBe('foo');
-  expect(liveSocket.viewLogger).toBe('foo');
-  expect(liveSocket.unloaded).toBe(false);
-  expect(liveSocket.bindingPrefix).toBe('phx-');
-  expect(liveSocket.activeElement).toBe(null);
-  expect(liveSocket.prevActive).toBe(null);
-});
-
-test('sets defaults with socket', async () => {
-  let liveSocket = new LiveSocket(new Socket('//example.org/chat'));
-  expect(liveSocket.socket).toBeDefined();
-  expect(liveSocket.socket.onOpen).toBeDefined();
-  expect(liveSocket.unloaded).toBe(false);
-  expect(liveSocket.bindingPrefix).toBe('phx-');
-  expect(liveSocket.activeElement).toBe(null);
-  expect(liveSocket.prevActive).toBe(null);
-});
-
-test('viewLogger', async () => {
-  let viewLogger = (view, kind, msg, obj) => {
-    expect(view.id).toBe('container');
-    expect(kind).toBe('updated');
-    expect(msg).toBe('');
-    expect(obj).toBe('\"<div>\"');
-  }
-  let liveSocket = new LiveSocket('/live', { viewLogger });
-  let view = new View(liveViewDOM(), liveSocket);
-  liveSocket.log(view, 'updated', () => ['', JSON.stringify('<div>')]);
-});
-
-test('connect', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
-
-  let socket = liveSocket.connect();
-
-  expect(liveSocket.views['container'].destroy).toBeDefined();
-  // i'm not sure why this is
-  expect(socket).toBeUndefined();
-});
-
-test('disconnect', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
-
-  liveSocket.connect();
-  liveSocket.disconnect();
-
-  expect(liveSocket.views['container'].destroy).toBeDefined();
-});
-
-test('channel', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
-
-  liveSocket.connect();
-  let channel = liveSocket.channel('lv:def456', () => {
-      return { session: this.getSession() };
+describe('LiveSocket', function() {
+  test('sets defaults', async () => {
+    let opts = { viewLogger: 'foo' };
+    let liveSocket = new LiveSocket('/live', opts);
+    expect(liveSocket.socket).toBeDefined();
+    expect(liveSocket.socket.onOpen).toBeDefined();
+    expect(liveSocket.opts.reconnectAfterMs).toBeDefined();
+    expect(liveSocket.opts.viewLogger).toBe('foo');
+    expect(liveSocket.viewLogger).toBe('foo');
+    expect(liveSocket.unloaded).toBe(false);
+    expect(liveSocket.bindingPrefix).toBe('phx-');
+    expect(liveSocket.activeElement).toBe(null);
+    expect(liveSocket.prevActive).toBe(null);
   });
 
-  expect(channel).toBeDefined();
-});
+  test('sets defaults with socket', async () => {
+    let liveSocket = new LiveSocket(new Socket('//example.org/chat'));
+    expect(liveSocket.socket).toBeDefined();
+    expect(liveSocket.socket.onOpen).toBeDefined();
+    expect(liveSocket.unloaded).toBe(false);
+    expect(liveSocket.bindingPrefix).toBe('phx-');
+    expect(liveSocket.activeElement).toBe(null);
+    expect(liveSocket.prevActive).toBe(null);
+  });
 
-test('getViewById', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
+  test('viewLogger', async () => {
+    let viewLogger = (view, kind, msg, obj) => {
+      expect(view.id).toBe('container');
+      expect(kind).toBe('updated');
+      expect(msg).toBe('');
+      expect(obj).toBe('\"<div>\"');
+    }
+    let liveSocket = new LiveSocket('/live', { viewLogger });
+    let view = new View(liveViewDOM(), liveSocket);
+    liveSocket.log(view, 'updated', () => ['', JSON.stringify('<div>')]);
+  });
 
-  liveSocket.connect();
+  test('connect', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
 
-  expect(liveSocket.getViewById('container').destroy).toBeDefined();
-});
+    let socket = liveSocket.connect();
 
-test('destroyViewById', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
+    expect(liveSocket.views['container'].destroy).toBeDefined();
+    // i'm not sure why this is
+    expect(socket).toBeUndefined();
+  });
 
-  liveSocket.connect();
+  test('disconnect', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
 
-  liveSocket.destroyViewById('container');
-  expect(liveSocket.getViewById('container')).toBeUndefined();
-});
+    liveSocket.connect();
+    liveSocket.disconnect();
 
-test('getBindingPrefix', async () => {
-  let liveSocket = new LiveSocket('/live');
+    expect(liveSocket.views['container'].destroy).toBeDefined();
+  });
 
-  expect(liveSocket.getBindingPrefix()).toEqual('phx-');
-});
+  test('channel', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
 
-test('getBindingPrefix custom', async () => {
-  let liveSocket = new LiveSocket('/live', { bindingPrefix: 'company-' });
+    liveSocket.connect();
+    let channel = liveSocket.channel('lv:def456', () => {
+        return { session: this.getSession() };
+    });
 
-  expect(liveSocket.getBindingPrefix()).toEqual('company-');
-});
+    expect(channel).toBeDefined();
+  });
 
-test('getActiveElement default before LiveSocket activeElement is set', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
+  test('getViewById', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
 
-  let input = liveSocket.document.querySelector('input');
-  input.focus();
+    liveSocket.connect();
 
-  expect(liveSocket.getActiveElement()).toEqual(input);
-});
+    expect(liveSocket.getViewById('container').destroy).toBeDefined();
+  });
 
-test('setActiveElement and getActiveElement', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
+  test('destroyViewById', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
 
-  let input = liveSocket.document.querySelector('input');
+    liveSocket.connect();
 
-  // .activeElement
-  liveSocket.setActiveElement(input);
-  expect(liveSocket.activeElement).toEqual(input);
-  expect(liveSocket.getActiveElement()).toEqual(input);
-});
+    liveSocket.destroyViewById('container');
+    expect(liveSocket.getViewById('container')).toBeUndefined();
+  });
 
-test('blurActiveElement', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
+  test('getBindingPrefix', async () => {
+    let liveSocket = new LiveSocket('/live');
 
-  let input = liveSocket.document.querySelector('input');
-  input.focus();
+    expect(liveSocket.getBindingPrefix()).toEqual('phx-');
+  });
 
-  expect(liveSocket.prevActive).toBeNull();
+  test('getBindingPrefix custom', async () => {
+    let liveSocket = new LiveSocket('/live', { bindingPrefix: 'company-' });
 
-  liveSocket.blurActiveElement();
-  // sets prevActive
-  expect(liveSocket.prevActive).toEqual(input);
-  expect(liveSocket.getActiveElement()).not.toEqual(input);
-});
+    expect(liveSocket.getBindingPrefix()).toEqual('company-');
+  });
 
-test('restorePreviouslyActiveFocus', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
+  test('getActiveElement default before LiveSocket activeElement is set', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
 
-  let input = liveSocket.document.querySelector('input');
-  input.focus();
+    let input = liveSocket.document.querySelector('input');
+    input.focus();
 
-  liveSocket.blurActiveElement();
-  expect(liveSocket.prevActive).toEqual(input);
-  expect(liveSocket.getActiveElement()).not.toEqual(input);
+    expect(liveSocket.getActiveElement()).toEqual(input);
+  });
 
-  // focus()
-  liveSocket.restorePreviouslyActiveFocus();
-  expect(liveSocket.prevActive).toEqual(input);
-  expect(liveSocket.getActiveElement()).toEqual(input);
-});
+  test('setActiveElement and getActiveElement', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
 
-test('dropActiveElement unsets prevActive', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
+    let input = liveSocket.document.querySelector('input');
 
-  liveSocket.connect();
+    // .activeElement
+    liveSocket.setActiveElement(input);
+    expect(liveSocket.activeElement).toEqual(input);
+    expect(liveSocket.getActiveElement()).toEqual(input);
+  });
 
-  let input = liveSocket.document.querySelector('input');
-  liveSocket.setActiveElement(input);
-  liveSocket.blurActiveElement();
-  expect(liveSocket.prevActive).toEqual(input);
+  test('blurActiveElement', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
 
-  let view = liveSocket.getViewById('container');
-  liveSocket.dropActiveElement(view);
-  expect(liveSocket.prevActive).toBeNull();
-  // this fails.  Is this correct?
-  // expect(liveSocket.getActiveElement()).not.toEqual(input);
-});
+    let input = liveSocket.document.querySelector('input');
+    input.focus();
 
-test('onViewError unsets prevActive', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
+    expect(liveSocket.prevActive).toBeNull();
 
-  liveSocket.connect();
+    liveSocket.blurActiveElement();
+    // sets prevActive
+    expect(liveSocket.prevActive).toEqual(input);
+    expect(liveSocket.getActiveElement()).not.toEqual(input);
+  });
 
-  let input = liveSocket.document.querySelector('input');
-  liveSocket.setActiveElement(input);
-  liveSocket.blurActiveElement();
-  expect(liveSocket.prevActive).toEqual(input);
+  test('restorePreviouslyActiveFocus', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
 
-  let view = liveSocket.getViewById('container');
-  liveSocket.onViewError(view);
-  expect(liveSocket.prevActive).toBeNull();
-  // this fails.  Is this correct?
-  // expect(liveSocket.getActiveElement()).not.toEqual(input);
+    let input = liveSocket.document.querySelector('input');
+    input.focus();
+
+    liveSocket.blurActiveElement();
+    expect(liveSocket.prevActive).toEqual(input);
+    expect(liveSocket.getActiveElement()).not.toEqual(input);
+
+    // focus()
+    liveSocket.restorePreviouslyActiveFocus();
+    expect(liveSocket.prevActive).toEqual(input);
+    expect(liveSocket.getActiveElement()).toEqual(input);
+  });
+
+  test('dropActiveElement unsets prevActive', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
+
+    liveSocket.connect();
+
+    let input = liveSocket.document.querySelector('input');
+    liveSocket.setActiveElement(input);
+    liveSocket.blurActiveElement();
+    expect(liveSocket.prevActive).toEqual(input);
+
+    let view = liveSocket.getViewById('container');
+    liveSocket.dropActiveElement(view);
+    expect(liveSocket.prevActive).toBeNull();
+    // this fails.  Is this correct?
+    // expect(liveSocket.getActiveElement()).not.toEqual(input);
+  });
+
+  test('onViewError unsets prevActive', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
+
+    liveSocket.connect();
+
+    let input = liveSocket.document.querySelector('input');
+    liveSocket.setActiveElement(input);
+    liveSocket.blurActiveElement();
+    expect(liveSocket.prevActive).toEqual(input);
+
+    let view = liveSocket.getViewById('container');
+    liveSocket.onViewError(view);
+    expect(liveSocket.prevActive).toBeNull();
+    // this fails.  Is this correct?
+    // expect(liveSocket.getActiveElement()).not.toEqual(input);
+  });
 });
--- assets/__tests__/view_test.js
+++ assets/__tests__/view_test.js
@@ -32,100 +32,102 @@ function dom() {
   return new JSDOM(`<!DOCTYPE html><body>${liveViewDOM().outerHTML}</body>`);
 }
 
-test('sets defaults', async () => {
-  let liveSocket = new LiveSocket('/live');
-  let el = liveViewDOM();
-  let view = new View(el, liveSocket);
-  expect(view.liveSocket).toBe(liveSocket);
-  expect(view.newChildrenAdded).toEqual(false);
-  expect(view.gracefullyClosed).toEqual(false);
-  expect(view.hasBoundUI).toEqual(false);
-  expect(view.prevKey).toEqual(null);
-  expect(view.parent).toBeUndefined();
-  expect(view.el).toBe(el);
-  expect(view.id).toEqual('container');
-  expect(view.view).toEqual('');
-  expect(view.bindingPrefix).toEqual('phx-');
-});
+describe('View', function() {
+  test('sets defaults', async () => {
+    let liveSocket = new LiveSocket('/live');
+    let el = liveViewDOM();
+    let view = new View(el, liveSocket);
+    expect(view.liveSocket).toBe(liveSocket);
+    expect(view.newChildrenAdded).toEqual(false);
+    expect(view.gracefullyClosed).toEqual(false);
+    expect(view.hasBoundUI).toEqual(false);
+    expect(view.prevKey).toEqual(null);
+    expect(view.parent).toBeUndefined();
+    expect(view.el).toBe(el);
+    expect(view.id).toEqual('container');
+    expect(view.view).toEqual('');
+    expect(view.bindingPrefix).toEqual('phx-');
+  });
 
-test('binding', async () => {
-  let liveSocket = new LiveSocket('/live');
-  let el = liveViewDOM();
-  let view = new View(el, liveSocket);
-  expect(view.binding('submit')).toEqual('phx-submit');
-});
+  test('binding', async () => {
+    let liveSocket = new LiveSocket('/live');
+    let el = liveViewDOM();
+    let view = new View(el, liveSocket);
+    expect(view.binding('submit')).toEqual('phx-submit');
+  });
 
-test('target', async () => {
-  let liveSocket = new LiveSocket('/live');
-  let el = liveViewDOM();
-  let view = new View(el, liveSocket);
-  expect(view.target(el)).toEqual(el);
-});
+  test('target', async () => {
+    let liveSocket = new LiveSocket('/live');
+    let el = liveViewDOM();
+    let view = new View(el, liveSocket);
+    expect(view.target(el)).toEqual(el);
+  });
 
-test('getSession', async () => {
-  let liveSocket = new LiveSocket('/live');
-  let el = liveViewDOM();
-  let view = new View(el, liveSocket);
-  expect(view.getSession()).toEqual('abc123');
-});
+  test('getSession', async () => {
+    let liveSocket = new LiveSocket('/live');
+    let el = liveViewDOM();
+    let view = new View(el, liveSocket);
+    expect(view.getSession()).toEqual('abc123');
+  });
 
-test('showLoader and hideLoader', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
-  let loader = liveSocket.document.createElement('span');
-  loader.style.display = 'none';
-  let phxView = liveSocket.document.querySelector('[data-phx-view]');
-  phxView.parentNode.insertBefore(loader, phxView.nextSibling);
-  let el = liveSocket.document.querySelector('[data-phx-view]');
+  test('showLoader and hideLoader', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
+    let loader = liveSocket.document.createElement('span');
+    loader.style.display = 'none';
+    let phxView = liveSocket.document.querySelector('[data-phx-view]');
+    phxView.parentNode.insertBefore(loader, phxView.nextSibling);
+    let el = liveSocket.document.querySelector('[data-phx-view]');
 
-  let view = new View(el, liveSocket);
-  view.showLoader();
-  expect(el.classList.contains('phx-disconnected')).toBeTruthy();
-  expect(loader.style.display).toEqual('block');
+    let view = new View(el, liveSocket);
+    view.showLoader();
+    expect(el.classList.contains('phx-disconnected')).toBeTruthy();
+    expect(loader.style.display).toEqual('block');
 
-  view.hideLoader();
-  expect(loader.style.display).toEqual('none');
-});
+    view.hideLoader();
+    expect(loader.style.display).toEqual('none');
+  });
 
-test('displayError', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
-  let loader = liveSocket.document.createElement('span');
-  let phxView = liveSocket.document.querySelector('[data-phx-view]');
-  phxView.parentNode.insertBefore(loader, phxView.nextSibling);
-  let el = liveSocket.document.querySelector('[data-phx-view]');
+  test('displayError', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
+    let loader = liveSocket.document.createElement('span');
+    let phxView = liveSocket.document.querySelector('[data-phx-view]');
+    phxView.parentNode.insertBefore(loader, phxView.nextSibling);
+    let el = liveSocket.document.querySelector('[data-phx-view]');
 
-  let view = new View(el, liveSocket);
-  view.displayError();
-  expect(el.classList.contains('phx-disconnected')).toBeTruthy();
-  expect(el.classList.contains('phx-error')).toBeTruthy();
-  expect(loader.style.display).toEqual('block');
-});
+    let view = new View(el, liveSocket);
+    view.displayError();
+    expect(el.classList.contains('phx-disconnected')).toBeTruthy();
+    expect(el.classList.contains('phx-error')).toBeTruthy();
+    expect(loader.style.display).toEqual('block');
+  });
 
-test('update', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
-  let el = liveViewDOM();
-  let updatedEl = {
-    static: ['<h2>'],
-    fingerprint: 123
-  };
+  test('update', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
+    let el = liveViewDOM();
+    let updatedEl = {
+      static: ['<h2>'],
+      fingerprint: 123
+    };
 
-  let view = new View(el, liveSocket);
+    let view = new View(el, liveSocket);
 
-  view.update(updatedEl);
+    view.update(updatedEl);
 
-  expect(view.el.firstChild.tagName).toBe('H2');
-  expect(view.newChildrenAdded).toBe(false);
-  expect(view.rendered).toBe(updatedEl);
-});
+    expect(view.el.firstChild.tagName).toBe('H2');
+    expect(view.newChildrenAdded).toBe(false);
+    expect(view.rendered).toBe(updatedEl);
+  });
 
-test('join', async () => {
-  let liveSocket = new LiveSocket('/live');
-  liveSocket.document = dom().window.document;
-  let el = liveViewDOM();
-  let view = new View(el, liveSocket);
+  test('join', async () => {
+    let liveSocket = new LiveSocket('/live');
+    liveSocket.document = dom().window.document;
+    let el = liveViewDOM();
+    let view = new View(el, liveSocket);
 
-  view.join();
-  // still need a few tests
+    view.join();
+    // still need a few tests
+  });
 });
