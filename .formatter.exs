92e4fd9fe15dcf639c97e7daef80f3cf30505dc3
Shritesh Bhattarai <shritesh@shritesh.com>
2019-03-28 04:42:00 -0500

    Add live in .formatter.exs. Fixes #124 (#127)
    * Format live/{2,3} without parens and add instructions

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,12 @@
 # Used by "mix format"
+locals_without_parens = [
+  # Phoenix.Router
+  live: 2,
+  live: 3
+]
+
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
