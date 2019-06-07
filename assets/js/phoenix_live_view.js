77bfe3c528e149686f4d3c74a7cd4b56da1866ec
TheMushr00m <admin@gcueto.com>
2019-06-07 13:31:21 -0500

    FIXES #234
    The change validates the type of element on which you are trying to apply the PHX_DISABLE_WITH logic to set a new text.

--- assets/js/phoenix_live_view.js
+++ assets/js/phoenix_live_view.js
@@ -531,7 +531,11 @@ let DOM = {
     Browser.all(form, `[${disableWith}]`, el => {
       let value = el.getAttribute(`${disableWith}-restore`)
       if(value){
-        el.innerText = value
+        if (el.nodeName == "INPUT") {
+            el.value = value
+        } else {
+            el.innerText = value
+        }
         el.removeAttribute(`${disableWith}-restore`)
       }
     })
