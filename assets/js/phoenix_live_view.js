415c7388d678887e688d58fc9735efb8b60aedc6
snewcomer <snewcomer24@gmail.com>
2019-08-31 14:11:49 -0700

    new build

--- assets/js/phoenix_live_view.js
+++ assets/js/phoenix_live_view.js
@@ -406,7 +406,6 @@ export class LiveSocket {
       let target = closestPhxBinding(e.target, click)
       let phxEvent = target && target.getAttribute(click)
       if(!phxEvent){ return }
-
       e.stopPropagation()
 
       let meta = {
