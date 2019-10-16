eea0efe3c83b03e182cdb7fa2b4cd47fb057100a
preciz <barna.kovacs.codes@gmail.com>
2019-10-16 20:15:13 +0200

    Fix minor typo

--- lib/phoenix_live_view.ex
+++ lib/phoenix_live_view.ex
@@ -161,7 +161,7 @@ defmodule Phoenix.LiveView do
         end
 
         def mount(%{id: id, current_user_id: user_id}, socket) do
-          temperature = Thermostat.get_user_reading(user_id, id) do
+          temperature = Thermostat.get_user_reading(user_id, id)
           {:ok, assign(socket, :temperature, temperature)}
         end
       end
