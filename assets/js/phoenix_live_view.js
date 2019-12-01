88b21ad6f8a41b94b332ec8b912c7c2be6971208
Felipe Renan <feelipe.renan@gmail.com>
2019-12-01 14:29:05 +0200

    Fix first render back button on live redirect
    Fixes https://github.com/phoenixframework/phoenix_live_view/issues/480
    
    The issue is that in the first render when users click on the browser
    back button, even though the URL changes to the previous one, the page
    doesn't show the previous page content. It works fine if the user clicks
    on browser next button and browser back button again, but it's not a
    good User experience.
    
    To fix that, I'm making sure the new location will be registered calling
    the function `registerNewLocation(window.location)` right after pushing
    state on live redirect.

--- assets/js/phoenix_live_view.js
+++ assets/js/phoenix_live_view.js
@@ -1195,6 +1195,7 @@ export class View {
   onLiveRedirect({to, kind}){
     this.href = to
     Browser.pushState(kind, {}, to)
+    this.liveSocket.registerNewLocation(window.location)
   }
 
   onRedirect({to, flash}){ Browser.redirect(to, flash) }
