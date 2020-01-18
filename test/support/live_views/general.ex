f5c18a5d5682850050072054397da024371aefca
José Valim <jose.valim@plataformatec.com.br>
2020-01-18 19:01:59 +0100

    Split into params fixtures

--- test/support/live_views/general.ex
+++ test/support/live_views/general.ex
@@ -209,59 +209,6 @@ defmodule Phoenix.LiveViewTest.ChildLive do
   end
 end
 
-defmodule Phoenix.LiveViewTest.ParamCounterLive do
-  use Phoenix.LiveView
-
-  def render(assigns) do
-    ~L"""
-    The value is: <%= @val %>
-    <%= if map_size(@params) > 0, do: inspect(@params) %>
-    connect: <%= inspect(@connect_params) %>
-    """
-  end
-
-  def mount(session, socket) do
-    on_handle_params = session["on_handle_params"]
-
-    {:ok,
-     assign(
-       socket,
-       val: 1,
-       connect_params: get_connect_params(socket) || %{},
-       test_pid: session["test_pid"],
-       on_handle_params: on_handle_params && :erlang.binary_to_term(on_handle_params)
-     )}
-  end
-
-  def handle_params(%{"from" => "handle_params"} = params, uri, socket) do
-    send(socket.assigns.test_pid, {:handle_params, uri, socket.assigns, params})
-    socket.assigns.on_handle_params.(assign(socket, :params, params))
-  end
-
-  def handle_params(params, uri, socket) do
-    send(socket.assigns.test_pid, {:handle_params, uri, socket.assigns, params})
-    {:noreply, assign(socket, :params, params)}
-  end
-
-  def handle_event("live_redirect", to, socket) do
-    {:noreply, live_redirect(socket, to: to)}
-  end
-
-  def handle_info({:set, var, val}, socket), do: {:noreply, assign(socket, var, val)}
-
-  def handle_info({:live_redirect, to}, socket) do
-    {:noreply, live_redirect(socket, to: to)}
-  end
-
-  def handle_call({:live_redirect, func}, _from, socket) do
-    func.(socket)
-  end
-
-  def handle_cast({:live_redirect, to}, socket) do
-    {:noreply, live_redirect(socket, to: to)}
-  end
-end
-
 defmodule Phoenix.LiveViewTest.OptsLive do
   use Phoenix.LiveView
 
