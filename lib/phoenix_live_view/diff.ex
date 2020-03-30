88e94da4b2e7dd88b87374f53e0226aabdab08f2
Sfusato <andrei@andreirus.com>
2020-03-30 19:21:08 +0200

    clear flash messages in stateless components. Closes #733 (#738)

--- lib/phoenix_live_view/diff.ex
+++ lib/phoenix_live_view/diff.ex
@@ -345,7 +345,10 @@ defmodule Phoenix.LiveView.Diff do
   end
 
   defp mount_component(socket, component) do
-    socket = configure_socket_for_component(socket, %{flash: %{}}, %{}, new_fingerprints())
+    socket =
+      configure_socket_for_component(socket, %{}, %{}, new_fingerprints())
+      |> Utils.assign(:flash, %{})
+
     Utils.maybe_call_mount!(socket, component, [socket])
   end
 
