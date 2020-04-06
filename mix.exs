6ef77bb4c0e4750c84bd3796cf4e3fa082fc0618
Chris McCord <chris@chrismccord.com>
2020-04-06 11:54:10 -0400

    Release 0.11.0

--- mix.exs
+++ mix.exs
@@ -87,7 +87,7 @@ defmodule Phoenix.LiveView.MixProject do
       licenses: ["MIT"],
       links: %{github: "https://github.com/phoenixframework/phoenix_live_view"},
       files:
-        ~w(assets/css assets/js lib priv) ++
+        ~w(assets/js lib priv) ++
           ~w(CHANGELOG.md LICENSE.md mix.exs package.json README.md)
     ]
   end
