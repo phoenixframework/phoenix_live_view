298820ed41d7cfc37c1abc58800072bf17ba0781
José Valim <jose.valim@dashbit.co>
2020-04-26 10:55:33 +0200

    Support different collocated template engines, closes #824

--- lib/phoenix_live_view/renderer.ex
+++ lib/phoenix_live_view/renderer.ex
@@ -3,10 +3,12 @@ defmodule Phoenix.LiveView.Renderer do
 
   defmacro __before_compile__(env) do
     render? = Module.defines?(env.module, {:render, 1})
-    template = template_path(env)
+    root = Path.dirname(env.file)
+    filename = template_filename(env)
+    templates = Phoenix.Template.find_all(root, filename)
 
-    case {render?, File.regular?(template)} do
-      {true, true} ->
+    case {render?, templates} do
+      {true, [template | _]} ->
         IO.warn(
           "ignoring template #{inspect(template)} because the LiveView " <>
             "#{inspect(env.module)} defines a render/1 function",
@@ -15,11 +17,13 @@ defmodule Phoenix.LiveView.Renderer do
 
         :ok
 
-      {true, false} ->
+      {true, []} ->
         :ok
 
-      {false, true} ->
-        ast = Phoenix.LiveView.Engine.compile(template, template_filename(env))
+      {false, [template]} ->
+        ext = template |> Path.extname() |> String.trim_leading(".") |> String.to_atom()
+        engine = Map.fetch!(Phoenix.Template.engines(), ext)
+        ast = engine.compile(template, filename)
 
         quote do
           @file unquote(template)
@@ -29,7 +33,16 @@ defmodule Phoenix.LiveView.Renderer do
           end
         end
 
-      {false, false} ->
+      {false, [_ | _]} ->
+        IO.warn(
+          "multiple templates were found for #{inspect(env.module)}: #{inspect(templates)}",
+          Macro.Env.stacktrace(env)
+        )
+
+        :ok
+
+
+      {false, []} ->
         message = ~s'''
         render/1 was not implemented for #{inspect(env.module)}.
 
@@ -41,7 +54,7 @@ defmodule Phoenix.LiveView.Renderer do
               """
             end
 
-        Or create a file at #{inspect(template)} with the LiveView template.
+        Or create a file at #{inspect(Path.join(root, filename <> ".leex"))} with the LiveView template.
         '''
 
         IO.warn(message, Macro.Env.stacktrace(env))
@@ -54,14 +67,7 @@ defmodule Phoenix.LiveView.Renderer do
     end
   end
 
-  defp template_path(env) do
-    env.file
-    |> Path.dirname()
-    |> Path.join(template_filename(env) <> ".leex")
-    |> Path.relative_to_cwd()
-  end
-
-  def template_filename(env) do
+  defp template_filename(env) do
     env.module
     |> Module.split()
     |> List.last()
