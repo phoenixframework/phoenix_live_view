d529e4084259b00411793af576fa38efe7914c36
José Valim <jose.valim@dashbit.co>
2020-06-19 23:06:45 +0200

    Fix phx-static not being sent for child views

--- assets/js/phoenix_live_view.js
+++ assets/js/phoenix_live_view.js
@@ -1628,7 +1628,13 @@ export class View {
       return this.applyJoinPatch(live_patch, html)
     }
 
-    let newChildren = DOM.findPhxChildrenInFragment(html, this.id).filter(c => this.joinChild(c))
+    let newChildren = DOM.findPhxChildrenInFragment(html, this.id).filter(toEl => {
+      let fromEl = toEl.id && this.el.querySelector(`#${toEl.id}`)
+      let phxStatic = fromEl && fromEl.getAttribute(PHX_STATIC)
+      if(phxStatic) toEl.setAttribute(PHX_STATIC, phxStatic)
+      return this.joinChild(toEl)
+    })
+
     if(newChildren.length === 0){
       if(this.parent){
         this.root.pendingJoinOps.push([this, () => this.applyJoinPatch(live_patch, html)])
