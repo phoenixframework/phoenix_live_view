0221bff2f8427275d0036fcab799b2e9e9cdb62d
Michael Crumm <mike@crumm.net>
2020-07-14 14:11:21 -0700

    Validate max_entries in config

--- test/phoenix_live_view/upload_config_test.exs
+++ test/phoenix_live_view/upload_config_test.exs
@@ -2,7 +2,7 @@ defmodule Phoenix.LiveView.UploadConfigTest do
   use ExUnit.Case, async: true
 
   alias Phoenix.LiveView
-  alias Phoenix.LiveView.UploadConfig
+  alias Phoenix.LiveView.{UploadConfig, UploadEntry}
 
   defp build_socket() do
     %LiveView.Socket{}
@@ -32,5 +32,42 @@ defmodule Phoenix.LiveView.UploadConfigTest do
       socket = LiveView.allow_upload(build_socket(), :avatar, extensions: :any)
       assert %UploadConfig{name: :avatar} = socket.assigns.uploads.avatar
     end
+  end
+
+  describe "put_entries/2" do
+    test "returns error when greater than max_entries are provided" do
+      socket = LiveView.allow_upload(build_socket(), :avatar, extensions: :any)
+      assert UploadConfig.put_entries(socket.assigns.uploads.avatar, [
+        build_client_entry(:avatar),
+        build_client_entry(:avatar)
+      ]) == {:error, :too_many_files}
+
+      socket = LiveView.allow_upload(build_socket(), :avatar, extensions: :any, max_entries: 2)
+      config = socket.assigns.uploads.avatar
+
+      assert {:ok, config} = UploadConfig.put_entries(config, [build_client_entry(:avatar)])
+      assert {:ok, config} = UploadConfig.put_entries(config, [build_client_entry(:avatar)])
+      assert UploadConfig.put_entries(config, [build_client_entry(:avatar)]) == {:error, :too_many_files}
+    end
+
+    test "puts list of valid entries" do
+      socket = LiveView.allow_upload(build_socket(), :avatar, extensions: :any)
+      config = socket.assigns.uploads.avatar
+      %{"name" => client_name} = client_entry = build_client_entry(:avatar)
+
+      {:ok, config} = UploadConfig.put_entries(config, [client_entry])
+      assert [%UploadEntry{client_name: ^client_name}] = config.entries
+    end
+  end
+
+  defp build_client_entry(name, attrs \\ %{}) do
+    attrs
+    |> Enum.into(%{
+      "name" => "#{name}_#{System.unique_integer([:positive, :monotonic])}",
+      "last_modified" => DateTime.utc_now() |> DateTime.to_unix(),
+      "size" => 1024,
+      "type" => "application/octet-stream"
+    })
+    |> Map.put_new_lazy("ref", &Phoenix.LiveView.Utils.random_id/0)
   end
 end
