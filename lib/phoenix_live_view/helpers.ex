98158bf66d2c3f67b8d2c9c5d6d1cea6b7207bb4
Leandro Cesquini Pereira <leandro@leandro.io>
2021-04-23 08:55:06 -0400

    Guard __component__

--- lib/phoenix_live_view/helpers.ex
+++ lib/phoenix_live_view/helpers.ex
@@ -250,9 +250,7 @@ defmodule Phoenix.LiveView.Helpers do
       <%= component(&MyApp.Weather.component/1, city: "Kraków") %>
 
   """
-  defmacro component(func, assigns \\ [], do_block \\ [])
-
-  defmacro component({:&, _, [{:/, _, [{_, _, _}, 1]}]} = func, assigns, do_block) do
+  defmacro component(func, assigns \\ [], do_block \\ []) do
     {do_block, assigns} =
       case {do_block, assigns} do
         {[do: do_block], _} -> {do_block, assigns}
@@ -269,22 +267,6 @@ defmodule Phoenix.LiveView.Helpers do
         unquote(inner_block)
       )
     end
-  end
-
-  defmacro component(_func, _assigns, _do_block) do
-    raise ArgumentError, """
-      component/3 expected an anonymous function with 1-arity
-
-      Please call component with a 1-arity function, for example:
-
-          <%= component &func/1 %>
-
-          def func(assigns) do
-            ~L\"""
-            Hello
-            \"""
-          end
-    """
   end
 
   defp rewrite_do(nil, opts, _caller), do: {opts, nil}
@@ -388,14 +370,31 @@ defmodule Phoenix.LiveView.Helpers do
 
   @doc false
   def __component__(func, assigns, inner)
-      when is_function(func) and is_list(assigns) or is_map(assigns) do
+      when is_function(func, 1) and is_list(assigns) or is_map(assigns) do
     assigns = Map.new(assigns)
     assigns = if inner, do: Map.put(assigns, :inner_block, inner), else: assigns
 
     func.(assigns)
   end
 
-  def __component__(func, assigns)
+  def __component__(func, assigns, _)
+      when is_function(func) and is_list(assigns) or is_map(assigns) do
+    raise ArgumentError, """
+      component/3 expected an anonymous function with 1-arity, got: #{inspect(func)}
+
+      Please call component with a 1-arity function, for example:
+
+          <%= component &func/1 %>
+
+          def func(assigns) do
+            ~L\"""
+            Hello
+            \"""
+          end
+    """
+  end
+
+  def __component__(func, assigns, _)
       when is_list(assigns) or is_map(assigns) do
     raise ArgumentError, "component/3 expected an anonymous function, got: #{inspect(func)}"
   end
