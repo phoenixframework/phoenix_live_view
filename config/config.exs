e5674ece9d0de7f658fbb0912a66281a037475c7
Chris McCord <chris@chrismccord.com>
2021-08-10 12:25:50 -0400

    Update esbuild config to fix warning

--- config/config.exs
+++ config/config.exs
@@ -5,14 +5,18 @@ config :phoenix, :trim_on_html_eex_engine, false
 config :logger, :level, :debug
 config :logger, :backends, []
 
+if Mix.env() == :dev do
+  esbuild = fn args ->
+    [
+      args: ~w(./js/phoenix_live_view --bundle) ++ args,
+      cd: Path.expand("../assets", __DIR__),
+      env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+    ]
+  end
 
-esbuild_base =  [
-  cd: Path.expand("../assets", __DIR__),
-  env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-]
-
-config :esbuild,
-  version: "0.12.15",
-  module: esbuild_base ++ [args: ~w(./js/phoenix_live_view --bundle --format=esm --sourcemap --outfile=../priv/static/phoenix_live_view.esm.js)],
-  cdn: esbuild_base ++ [args: ~w(./js/phoenix_live_view --bundle --format=iife --target=es2016 --global-name=LiveView --outfile=../priv/static/phoenix_live_view.js)],
-  cdn_min: esbuild_base ++ [args: ~w(./js/phoenix_live_view --bundle --format=iife --target=es2016 --global-name=LiveView --minify --outfile=../priv/static/phoenix_live_view.min.js)]
+  config :esbuild,
+    version: "0.12.15",
+    module: esbuild.(~w(--format=esm --sourcemap --outfile=../priv/static/phoenix_live_view.esm.js)),
+    cdn: esbuild.(~w(--format=iife --target=es2016 --global-name=LiveView --outfile=../priv/static/phoenix_live_view.js)),
+    cdn_min: esbuild.(~w(--format=iife --target=es2016 --global-name=LiveView --minify --outfile=../priv/static/phoenix_live_view.min.js))
+end
