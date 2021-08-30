99183c85cd58b89d0f34d46fb4958f868e82d063
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-08-30 21:30:52 +0300

    update js snippet for 0.16

--- lib/phoenix_live_view.ex
+++ lib/phoenix_live_view.ex
@@ -178,7 +178,7 @@ defmodule Phoenix.LiveView do
   app.js file, you should find the following:
 
       import {Socket} from "phoenix"
-      import LiveSocket from "phoenix_live_view"
+      import {LiveSocket} from "phoenix_live_view"
 
       let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
       let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
