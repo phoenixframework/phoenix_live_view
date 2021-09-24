c82fccfcf7fb550741090fdf70cfaf9409ae34ae
Chris McCord <chris@chrismccord.com>
2021-09-24 10:58:09 -0400

    Spaces

--- assets/js/phoenix_live_view/cmd.js
+++ assets/js/phoenix_live_view/cmd.js
@@ -1,67 +1,67 @@
 import DOM from "./dom"
 
 let Cmd = {
-	exec(eventType, phxEvent, view, el, [defaultKind, defaultArgs]){
-		let commands = phxEvent.charAt(0) === "{" ?
-		  JSON.parse(phxEvent) : [[defaultKind, defaultArgs]]
+  exec(eventType, phxEvent, view, el, [defaultKind, defaultArgs]){
+    let commands = phxEvent.charAt(0) === "{" ?
+      JSON.parse(phxEvent) : [[defaultKind, defaultArgs]]
 
-		commands.forEach(([kind, args]) => {
-			if(kind === defaultKind && defaultArgs.data){
-				args.data = Object.assign(args.data || {}, defaultArgs.data)
-			}
-			this[`exec_${kind}`](eventType, phxEvent, view, el, args)
-		})
-	},
+    commands.forEach(([kind, args]) => {
+      if(kind === defaultKind && defaultArgs.data){
+        args.data = Object.assign(args.data || {}, defaultArgs.data)
+      }
+      this[`exec_${kind}`](eventType, phxEvent, view, el, args)
+    })
+  },
 
-	// private
+  // private
 
-	// commands
+  // commands
 
-	exec_dispatch(eventType, phxEvent, view, sourceEl, {to, event, detail}){
-		DOM.all(document, to, el => DOM.dispatchEvent(el, event, detail))
-	},
+  exec_dispatch(eventType, phxEvent, view, sourceEl, {to, event, detail}){
+    DOM.all(document, to, el => DOM.dispatchEvent(el, event, detail))
+  },
 
-	exec_push(eventType, phxEvent, view, sourceEl, {event, data, target} = meta){
-		let phxTarget = target || sourceEl.getAttribute(view.binding("target")) || sourceEl
-		view.withinTargets(phxTarget, (targetView, targetCtx) => {
-			if(eventType === "click"){
+  exec_push(eventType, phxEvent, view, sourceEl, {event, data, target} = meta){
+    let phxTarget = target || sourceEl.getAttribute(view.binding("target")) || sourceEl
+    view.withinTargets(phxTarget, (targetView, targetCtx) => {
+      if(eventType === "click"){
         targetView.pushClick(sourceEl, event || phxEvent, targetCtx, data)
-			} else if(eventType === "change"){
-				let {newCid, _target} = meta
-		  	targetView.pushInput(sourceEl, targetCtx, newCid, phxEvent || event, _target)
-			} else if(eventType === "submit"){
+      } else if(eventType === "change"){
+        let {newCid, _target} = meta
+        targetView.pushInput(sourceEl, targetCtx, newCid, phxEvent || event, _target)
+      } else if(eventType === "submit"){
         targetView.submitForm(sourceEl, targetCtx, phxEvent || event)
-			} else {
-			  targetView.pushEvent(eventType, sourceEl, targetCtx, event || phxEvent, data)
-			}
-		})
-	},
+      } else {
+        targetView.pushEvent(eventType, sourceEl, targetCtx, event || phxEvent, data)
+      }
+    })
+  },
 
-	exec_add_class(eventType, phxEvent, view, sourceEl, {to, names}){
-		if(to){
-			DOM.all(document, to, el => this.addOrRemoveClasses(el, names, []))
-		} else {
-			this.addOrRemoveClasses(sourceEl, names, [])
-		}
-	},
+  exec_add_class(eventType, phxEvent, view, sourceEl, {to, names}){
+    if(to){
+      DOM.all(document, to, el => this.addOrRemoveClasses(el, names, []))
+    } else {
+      this.addOrRemoveClasses(sourceEl, names, [])
+    }
+  },
 
-	exec_remove_class(eventType, phxEvent, view, sourceEl, {to, names}){
-		if(to){
-			DOM.all(document, to, el => this.addOrRemoveClasses(el, [], names))
-		} else {
+  exec_remove_class(eventType, phxEvent, view, sourceEl, {to, names}){
+    if(to){
+      DOM.all(document, to, el => this.addOrRemoveClasses(el, [], names))
+    } else {
       this.addOrRemoveClasses(sourceEl, [], names)
-		}
-	},
+    }
+  },
 
-	exec_toggle(eventType, phxEvent, view, sourceEl, {to}){
-		if(to){
-		  DOM.all(document, to, el => this.toggle(el, "inline-block"))
-		} else {
-			this.toggle(sourceEl, "inline-block")
-		}
-	},
+  exec_toggle(eventType, phxEvent, view, sourceEl, {to}){
+    if(to){
+      DOM.all(document, to, el => this.toggle(el, "inline-block"))
+    } else {
+      this.toggle(sourceEl, "inline-block")
+    }
+  },
 
-	// utils for commands
+  // utils for commands
 
   toggle(el, defaultDisplay){
     let newDisplay = el.style.display === "none" ? defaultDisplay : "none"
@@ -69,14 +69,14 @@ let Cmd = {
   },
 
   addOrRemoveClasses(el, addClasses, removeClasses){
-		let [prevAdds, prevRemoves] = DOM.getSticky(el, "classes", [[], []])
-		let newAdds = prevAdds.filter(name => removeClasses.indexOf(name) >= 0).concat(addClasses)
-		let newRemoves = prevRemoves.filter(name => addClasses.indexOf(name) >= 0).concat(removeClasses)
+    let [prevAdds, prevRemoves] = DOM.getSticky(el, "classes", [[], []])
+    let newAdds = prevAdds.filter(name => removeClasses.indexOf(name) >= 0).concat(addClasses)
+    let newRemoves = prevRemoves.filter(name => addClasses.indexOf(name) >= 0).concat(removeClasses)
 
     DOM.putSticky(el, "classes", currentEl => {
       newAdds.forEach(name => currentEl.classList.add(name))
       newRemoves.forEach(name => currentEl.classList.remove(name))
-			return [newAdds, newRemoves]
+      return [newAdds, newRemoves]
     })
   }
 }
