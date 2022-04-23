2ae6be78c55ce9682904a336e04cc4410642eac7
Leandro Cesquini Pereira <leandro@leandro.io>
2022-04-23 13:11:47 -0400

    HTMLFormatter - add a note about inline comments
    Close #1989

--- lib/phoenix_live_view/html_formatter.ex
+++ lib/phoenix_live_view/html_formatter.ex
@@ -175,6 +175,12 @@ defmodule Phoenix.LiveView.HTMLFormatter do
   ```html
   <textarea>My content</textarea>
   ```
+
+  ### Inline comments <%# comment %>
+
+  Inline comments `<%# comment %>` were deprecated and the formatter will remove them out silently
+  from the templates, consider changing manually to the multi-line comment `<%!-- comment --%>` on
+  Elixir v1.14+ or the regular line comment `<%= # comment %>`.
   """
 
   alias Phoenix.LiveView.HTMLAlgebra
