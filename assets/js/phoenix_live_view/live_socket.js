776a612d5ce6db54fc8a6bbad310d4cabbd64af7
Matt Stubbs <22266+msmithstubbs@users.noreply.github.com>
2022-05-10 22:49:00 +1000

    Update sessionStorage docs InMemoryStorage example (#2021)
    This is a very small change that ensures the `InMemoryStorage` example code returns `null` when a key is not found.
    
    LiveView stores data in localStore in JSON, and a missing key will return `null`. The current `InMemoryStorageClass` example will return `undefined` which is not valid JSON and cause an exception[1]. 
    
    
    [1]: `null` is [considered valid JSON in browserland](https://stackoverflow.com/questions/46464447/json-parse-allows-null-as-value]

--- assets/js/phoenix_live_view/live_socket.js
+++ assets/js/phoenix_live_view/live_socket.js
@@ -62,7 +62,7 @@
  *
  *     class InMemoryStorage {
  *       constructor() { this.storage = {} }
- *       getItem(keyName) { return this.storage[keyName] }
+ *       getItem(keyName) { return this.storage[keyName] || null }
  *       removeItem(keyName) { delete this.storage[keyName] }
  *       setItem(keyName, keyValue) { this.storage[keyName] = keyValue }
  *     }
