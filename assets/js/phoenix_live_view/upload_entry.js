15b13ba12e6d5df37ca3240062f73ffc2639a0ea
Michael Crumm <mike@crumm.net>
2022-06-07 14:38:57 -0700

    Cleanup after upload entry error, closes #2037 (#2054)
    In order to ensure failed entries do not interfere with
    further form actions, entries stop listening for update
    events once error() is invoked.

--- assets/js/phoenix_live_view/upload_entry.js
+++ assets/js/phoenix_live_view/upload_entry.js
@@ -69,6 +69,7 @@ export default class UploadEntry {
   isDone(){ return this._isDone }
 
   error(reason = "failed"){
+    this.fileEl.removeEventListener(PHX_LIVE_FILE_UPDATED, this._onElUpdated)
     this.view.pushFileProgress(this.fileEl, this.ref, {error: reason})
     LiveUploader.clearFiles(this.fileEl)
   }
