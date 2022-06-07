b00aef3aec31871020392f63bf99add3446ec18b
Chris McCord <chris@chrismccord.com>
2022-06-07 13:59:52 -0400

    Allow sharing LV socket with regular channel socket

--- lib/phoenix_live_view/socket.ex
+++ lib/phoenix_live_view/socket.ex
@@ -17,8 +17,19 @@ defmodule Phoenix.LiveView.Socket do
 
   This is typically mounted directly in your endpoint.
 
-      socket "/live", Phoenix.LiveView.Socket
+      socket "/live", Phoenix.LiveView.Socket,
+        websocket: [connect_info: [session: @session_options]]
 
+  To share an underlying transport connection between regular
+  Phoenix channels and LiveView processes, `use Phoenix.LiveView.Socket`
+  form your own `MyAppWeb.UserSocket` module.
+
+  Next, declare your `channel` definitions and optional `connect/3`, and
+  `id/1` callbacks to handle your channel specific needs, then mount
+  your own socket in your endpoint:
+
+      socket "/live", MyAppWeb.UserSocket,
+        websocket: [connect_info: [session: @session_options]]
   """
   use Phoenix.Socket
 
@@ -82,4 +93,53 @@ defmodule Phoenix.LiveView.Socket do
 
   @impl Phoenix.Socket
   def id(socket), do: socket.private.connect_info[:session]["live_socket_id"]
+
+  defmacro __using__(_opts) do
+    quote do
+      use Phoenix.Socket
+
+      channel "lvu:*", Phoenix.LiveView.UploadChannel
+      channel "lv:*", Phoenix.LiveView.Channel
+
+      @phoenix_connect false
+      @phoenix_id false
+
+      @before_compile unquote(__MODULE__)
+      @on_definition unquote(__MODULE__)
+    end
+  end
+
+  def __on_definition__(env, :def, :connect, [_, _, _], _guards, _body) do
+    Module.put_attribute(env.module, :phoenix_connect, true)
+  end
+
+  def __on_definition__(env, :def, :id, [_], _guards, _body) do
+    Module.put_attribute(env.module, :phoenix_id, true)
+  end
+
+  def __on_definition__(_env, _kind, _name, _args, _guards, _body), do: :ok
+
+  defmacro __before_compile__(_env) do
+    quote do
+      if @phoenix_connect do
+        defoverridable connect: 3
+
+        def connect(params, %Phoenix.Socket{} = socket, connect_info) do
+          case super(params, socket, connect_info) do
+            {:ok, %Phoenix.Socket{} = new_socket} ->
+              Phoenix.LiveView.Socket.connect(params, new_socket, connect_info)
+
+            other ->
+              other
+          end
+        end
+      else
+        defdelegate connect(params, socket, info), to: unquote(__MODULE__)
+      end
+
+      unless @phoenix_id do
+        defdelegate id(socket), to: unquote(__MODULE__)
+      end
+    end
+  end
 end
