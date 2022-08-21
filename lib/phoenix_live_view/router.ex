5eff5cc0e32459522299d78c170a62a97f88b8d9
Chris McCord <chris@chrismccord.com>
2022-08-21 10:42:18 -0400

    Forward warn on verify to phoenix router get

--- lib/phoenix_live_view/router.ex
+++ lib/phoenix_live_view/router.ex
@@ -355,7 +355,7 @@ defmodule Phoenix.LiveView.Router do
         %{name: :default, extra: %{session: %{}}, vsn: session_vsn(router)}
 
     live_view = Phoenix.Router.scoped_alias(router, live_view)
-    {private, metadata, opts} = validate_live_opts!(opts)
+    {private, metadata, warn_on_verify, opts} = validate_live_opts!(opts)
 
     opts =
       opts
@@ -373,6 +373,7 @@ defmodule Phoenix.LiveView.Router do
     {as_action,
      alias: false,
      as: as_helper,
+     warn_on_verify: warn_on_verify,
      private: Map.put(private, :phoenix_live_view, {live_view, opts, live_session}),
      metadata: metadata}
   end
@@ -380,6 +381,7 @@ defmodule Phoenix.LiveView.Router do
   defp validate_live_opts!(opts) do
     {private, opts} = Keyword.pop(opts, :private, %{})
     {metadata, opts} = Keyword.pop(opts, :metadata, %{})
+    {warn_on_verify, opts} = Keyword.pop(opts, :warn_on_verify, true)
 
     Enum.each(opts, fn
       {:container, {tag, attrs}} when is_atom(tag) and is_list(attrs) ->
@@ -416,7 +418,7 @@ defmodule Phoenix.LiveView.Router do
         """
     end)
 
-    {private, metadata, opts}
+    {private, metadata, warn_on_verify, opts}
   end
 
   defp inferred_as(live_view, as, nil), do: {as || :live, live_view}
