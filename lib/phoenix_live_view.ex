e517599c50bb822d9cadaa608691406d6d8d9b99
Tom Taylor <tom@tomtaylor.co.uk>
2022-09-26 10:58:06 +0100

    Fix documentation for router hooks (#2228)
    `assign` (used in line 581) is not imported from `Phoenix.LiveView` since 0.18, instead this is available in `Phoenix.Component`.

--- lib/phoenix_live_view.ex
+++ lib/phoenix_live_view.ex
@@ -575,6 +575,7 @@ defmodule Phoenix.LiveView do
         Ensures common `assigns` are applied to all LiveViews attaching this hook.
         "\""
         import Phoenix.LiveView
+        import Phoenix.Component
 
         def on_mount(:default, _params, _session, socket) do
           {:cont, assign(socket, :page_title, "DemoWeb")}
