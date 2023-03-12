c102b6093aabd60e28097e4cc2234a5a437a0060
Jonathan Caprell <jbcaprell@diacritical.net>
2023-03-12 05:02:07 -0400

    Update `.formatter.exs` for Phoenix 1.7 release (#2520)

--- .formatter.exs
+++ .formatter.exs
@@ -1,17 +1,4 @@
-locals_without_parens = [
-  attr: 2,
-  attr: 3,
-  embed_templates: 1,
-  embed_templates: 2,
-  slot: 1,
-  slot: 2,
-  slot: 3
-]
-
 [
   import_deps: [:phoenix],
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
-  # TODO: Remove these on Phoenix v1.7 since Phoenix provides them already
-  locals_without_parens: locals_without_parens,
-  export: [locals_without_parens: locals_without_parens]
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"]
 ]
