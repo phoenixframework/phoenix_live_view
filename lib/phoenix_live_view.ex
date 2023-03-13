4c106d02767fe6b94c63d7ceb7c603586a834f21
Max Marcon <maxmarcon@gmx.net>
2023-03-13 06:54:08 +0100

    docs make clear that hooks are not supported in LiveComponents

--- lib/phoenix_live_view.ex
+++ lib/phoenix_live_view.ex
@@ -1401,6 +1401,8 @@ defmodule Phoenix.LiveView do
   > Note: This function is for server-side lifecycle callbacks.
   > For client-side hooks, see the
   > [JS Interop guide](js-interop.html#client-hooks).
+    
+  > Note: Hooks are not supported in LiveComponents.
 
   Hooks provide a mechanism to tap into key stages of the LiveView
   lifecycle in order to bind/update assigns, intercept events,
