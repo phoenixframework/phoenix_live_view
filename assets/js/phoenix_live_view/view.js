55ace4c22e65f1e04c3e396ad2824707e012762d
Chris McCord <chris@chrismccord.com>
2023-05-15 13:05:22 -0400

    Only cancel form submission if awaiting previous submission

--- assets/js/phoenix_live_view/view.js
+++ assets/js/phoenix_live_view/view.js
@@ -998,7 +998,7 @@ export default class View {
           cid: cid
         }, onReply)
       })
-    } else if(!formEl.hasAttribute(PHX_REF)){
+    } else if(!(formEl.hasAttribute(PHX_REF) && formEl.classList.contains("phx-submit-loading"))){
       let meta = this.extractMeta(formEl)
       let formData = serializeForm(formEl, {submitter, ...meta})
       this.pushWithReply(refGenerator, "event", {
