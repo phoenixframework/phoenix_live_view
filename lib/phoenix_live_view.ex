4a41c9fbdc30c5e88c63ebdbf61deeecb346206d
mavenraven <26941+mavenraven@users.noreply.github.com>
2023-05-19 19:31:50 -0400

    Fixes typo in LiveView docs.
    I noticed this while reading through the LiveView docs, and it confused me for a few minutes.

--- lib/phoenix_live_view.ex
+++ lib/phoenix_live_view.ex
@@ -83,7 +83,7 @@ defmodule Phoenix.LiveView do
         # In Phoenix v1.6+ apps, the line below should be: use MyAppWeb, :live_view
         use Phoenix.LiveView
 
-        def render(assigns) do
+        def render(socket) do
           ~H"""
           Current temperature: <%= @temperature %>
           """
