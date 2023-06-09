b2ff167634adc6046cac9f10071d22ad122dafb6
Olexandr Maslo <18098273+maslowalex@users.noreply.github.com>
2023-06-09 19:45:26 +0300

    Add disabled to accepted file input attributes (#2676)

--- lib/phoenix_component.ex
+++ lib/phoenix_component.ex
@@ -2751,7 +2751,7 @@ defmodule Phoenix.Component do
       "the optional override for the accept attribute. Defaults to :accept specified by allow_upload"
   )
 
-  attr.(:rest, :global, include: ~w(webkitdirectory required))
+  attr.(:rest, :global, include: ~w(webkitdirectory required disabled))
 
   def live_file_input(%{upload: upload} = assigns) do
     assigns = assign_new(assigns, :accept, fn -> upload.accept != :any && upload.accept end)
