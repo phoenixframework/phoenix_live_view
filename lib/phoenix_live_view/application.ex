706b657df253f5d83e9b8e20cc82c99ae6ec909c
Chris McCord <chris@chrismccord.com>
2023-08-04 16:52:11 -0400

    Don't need this

--- lib/phoenix_live_view/application.ex
+++ lib/phoenix_live_view/application.ex
@@ -6,11 +6,6 @@ defmodule Phoenix.LiveView.Application do
   @impl true
   def start(_type, _args) do
     Phoenix.LiveView.Logger.install()
-
-    children = [
-      {Task.Supervisor, name: Phoenix.LiveView.TaskSup}
-    ]
-
-    Supervisor.start_link(children, strategy: :one_for_one, name: Phoenix.LiveView.Supervisor)
+    Supervisor.start_link([], strategy: :one_for_one, name: Phoenix.LiveView.Supervisor)
   end
 end
