33411265811888e7b120c6b8828d7a7d00711c13
José Valim <jose.valim@dashbit.co>
2023-10-06 12:12:02 +0200

    Depend on latest Plug and use more efficient query decoding

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule Phoenix.LiveView.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.6.15 or ~> 1.7.0"},
+      {:plug, "~> 1.15"},
       {:phoenix_view, "~> 2.0", optional: true},
       {:phoenix_template, "~> 1.0"},
       {:phoenix_html, "~> 3.3"},
