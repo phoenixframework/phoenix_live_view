phoenixframework/phoenix_live_view

commit 847c704bac5cdd8ad1f596acc2935e07929b6fa4

Support form submitter (#2490)

assets/js/phoenix_live_view/view.js
@@ -51,8 +51,18 @@ import Rendered from "./rendered"
 import ViewHook from "./view_hook"
 import JS from "./js"
 
-let serializeForm = (form, meta, onlyNames = []) => {
+let serializeForm = (form, metadata, onlyNames = []) => {
+  let {submitter, ...meta} = metadata
+
+  // TODO: Replace with `new FormData(form, submitter)` when supported by latest browsers,
+  //       and mention `formdata-submitter-polyfill` in the docs.
   let formData = new FormData(form)
+
+  // TODO: Remove when FormData constructor supports the submitter argument.
+  if (submitter && submitter.form && submitter.form === form){
+    formData.append(submitter.name, submitter.value)
+  }
+
   let toRemove = []
 
   formData.forEach((val, key, _index) => {
@@ -954,18 +964,18 @@ export default class View {
     return this.putRef([formEl].concat(disables).concat(buttons).concat(inputs), "submit", opts)
   }
 
-  pushFormSubmit(formEl, targetCtx, phxEvent, opts, onReply){
+  pushFormSubmit(formEl, targetCtx, phxEvent, submitter, opts, onReply){
     let refGenerator = () => this.disableForm(formEl, opts)
     let cid = this.targetComponentID(formEl, targetCtx)
     if(LiveUploader.hasUploadsInProgress(formEl)){
       let [ref, _els] = refGenerator()
-      let push = () => this.pushFormSubmit(formEl, targetCtx, phxEvent, opts, onReply)
+      let push = () => this.pushFormSubmit(formEl, submitter, targetCtx, phxEvent, opts, onReply)
       return this.scheduleSubmit(formEl, ref, opts, push)
     } else if(LiveUploader.inputsAwaitingPreflight(formEl).length > 0){
       let [ref, els] = refGenerator()
       let proxyRefGen = () => [ref, els, opts]
       this.uploadFiles(formEl, targetCtx, ref, cid, (_uploads) => {
-        let formData = serializeForm(formEl, {})
+        let formData = serializeForm(formEl, {submitter})
         this.pushWithReply(proxyRefGen, "event", {
           type: "form",
           event: phxEvent,
@@ -974,7 +984,7 @@ export default class View {
         }, onReply)
       })
     } else {
-      let formData = serializeForm(formEl, {})
+      let formData = serializeForm(formEl, {submitter})
       this.pushWithReply(refGenerator, "event", {
         type: "form",
         event: phxEvent,
@@ -1127,13 +1137,13 @@ export default class View {
       (!parentViewEl && this.isDead)
   }
 
-  submitForm(form, targetCtx, phxEvent, opts = {}){
+  submitForm(form, targetCtx, phxEvent, submitter, opts = {}){
     DOM.putPrivate(form, PHX_HAS_SUBMITTED, true)
     let phxFeedback = this.liveSocket.binding(PHX_FEEDBACK_FOR)
     let inputs = Array.from(form.elements)
     inputs.forEach(input => DOM.putPrivate(input, PHX_HAS_SUBMITTED, true))
     this.liveSocket.blurActiveElement(this)
-    this.pushFormSubmit(form, targetCtx, phxEvent, opts, () => {
+    this.pushFormSubmit(form, targetCtx, phxEvent, submitter, opts, () => {
       inputs.forEach(input => DOM.showError(input, phxFeedback))
       this.liveSocket.restorePreviouslyActiveFocus()
     })

assets/js/phoenix_live_view/js.js
@@ -47,7 +47,8 @@ let JS = {
         if(_target){ pushOpts._target = _target }
         targetView.pushInput(sourceEl, targetCtx, newCid, event || phxEvent, pushOpts, callback)
       } else if(eventType === "submit"){
-        targetView.submitForm(sourceEl, targetCtx, event || phxEvent, pushOpts)
+        let {submitter} = args
+        targetView.submitForm(sourceEl, targetCtx, event || phxEvent, submitter, pushOpts)
       } else {
         targetView.pushEvent(eventType, sourceEl, targetCtx, event || phxEvent, data, pushOpts)
       }

assets/js/phoenix_live_view/live_socket.js
@@ -822,7 +822,7 @@ export default class LiveSocket {
       e.preventDefault()
       e.target.disabled = true
       this.withinOwners(e.target, view => {
-        JS.exec("submit", phxEvent, view, e.target, ["push", {}])
+        JS.exec("submit", phxEvent, view, e.target, ["push", {submitter: e.submitter}])
       })
     }, false)
 

assets/test/view_test.js
@@ -250,6 +250,32 @@ describe("View + DOM", function(){
       view.submitForm(form, form, {target: form})
     })
 
+    test("payload includes submitter when provided", function(){
+      let liveSocket = new LiveSocket("/live", Socket)
+      let el = liveViewDOM()
+      let form = el.querySelector("form")
+      let btn = document.createElement("button")
+      btn.setAttribute("type", "submit")
+      btn.setAttribute("name", "btnName")
+      btn.setAttribute("value", "btnValue")
+      form.appendChild(btn)
+
+      let view = simulateJoinedView(el, liveSocket)
+      let channelStub = {
+        push(_evt, payload, _timeout){
+          expect(payload.type).toBe("form")
+          expect(payload.event).toBeDefined()
+          expect(payload.value).toBe("increment=1&note=2&btnName=btnValue")
+          return {
+            receive(){ return this }
+          }
+        }
+      }
+
+      view.channel = channelStub
+      view.submitForm(form, form, {target: form}, btn)
+    })
+
     test("disables elements after submission", function(){
       let liveSocket = new LiveSocket("/live", Socket)
       let el = liveViewDOM()

commit a49977379cc21329fb3c77353edfabde26a09d70

Fix form submissions that wait for uploads to complete
This fixes an issue we discovered at Felt, which was introduced in #2490.

The symptoms were as follows:

- Given a form with an image upload, if you waited until the upload completed before submitting the form, the submission would work as expected.
- However, if you submitted before the upload completed, you'd see the submission correctly wait until the upload hit 100%, then you'd get a JavaScript crash and the app would go dead.

The JavaScript console produced an error like this in release:

```
TypeError: r.hasAttribute is not a function. (In 'r.hasAttribute("name")', 'r.hasAttribute' is undefined)
```

The error in dev, of course, was more descriptive:

```
submitter.hasAttribute is not a function
Call Stack
 serializeForm  node_modules/phoenix_live_view/priv/static/phoenix_live_view.esm.js:2402:30
 pushFormSubmit  node_modules/phoenix_live_view/priv/static/phoenix_live_view.esm.js:3308:35
 push  node_modules/phoenix_live_view/priv/static/phoenix_live_view.esm.js:3293:29
 triggerAwaitingSubmit  node_modules/phoenix_live_view/priv/static/phoenix_live_view.esm.js:3236:7
 pushInput/  node_modules/phoenix_live_view/priv/static/phoenix_live_view.esm.js:3223:18
 uploadFiles/  node_modules/phoenix_live_view/priv/static/phoenix_live_view.esm.js:3325:11
 initAdapterUpload/this._entries  node_modules/phoenix_live_view/priv/static/phoenix_live_view.esm.js:895:16
 onDone/this._onDone  node_modules/phoenix_live_view/priv/static/phoenix_live_view.esm.js:769:7
 progress/<  node_modules/phoenix_live_view/priv/static/phoenix_live_view.esm.js:745:16
 finish  node_modules/phoenix_live_view/priv/static/phoenix_live_view.esm.js:3034:18
```

Inspecting the code, it looks like the argument order got swapped in the case where uploads are in progress at form submission time.

I'm not sure how to write an adequate test to cover this, but I can confirm that the fix when applied to my codebase does indeed fix the issue.

assets/js/phoenix_live_view/view.js
@@ -995,7 +995,7 @@ export default class View {
     let cid = this.targetComponentID(formEl, targetCtx)
     if(LiveUploader.hasUploadsInProgress(formEl)){
       let [ref, _els] = refGenerator()
-      let push = () => this.pushFormSubmit(formEl, submitter, targetCtx, phxEvent, opts, onReply)
+      let push = () => this.pushFormSubmit(formEl, targetCtx, phxEvent, submitter, opts, onReply)
       return this.scheduleSubmit(formEl, ref, opts, push)
     } else if(LiveUploader.inputsAwaitingPreflight(formEl).length > 0){
       let [ref, els] = refGenerator()