1ff348907d2c1ea229119df298b97ff8f859a0dc
Steffen Deusch <steffen@deusch.me>
2024-01-12 22:32:42 +0100

    align spaces

--- test/e2e/utils.js
+++ test/e2e/utils.js
@@ -2,13 +2,13 @@ const { expect } = require("@playwright/test");
 
 // a helper function to wait until the LV has no pending events
 const syncLV = async (page) => {
-    const promises = [
-        expect(page.locator(".phx-connected").first()).toBeVisible(),
-        expect(page.locator(".phx-change-loading")).toHaveCount(0),
-        expect(page.locator(".phx-click-loading")).toHaveCount(0),
-        expect(page.locator(".phx-submit-loading")).toHaveCount(0),
-    ];
-    return Promise.all(promises);
+  const promises = [
+    expect(page.locator(".phx-connected").first()).toBeVisible(),
+    expect(page.locator(".phx-change-loading")).toHaveCount(0),
+    expect(page.locator(".phx-click-loading")).toHaveCount(0),
+    expect(page.locator(".phx-submit-loading")).toHaveCount(0),
+  ];
+  return Promise.all(promises);
 };
 
 module.exports = { syncLV };
