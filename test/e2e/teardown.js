c2b8cbff3192ed91207913308fa503ba7b5b3302
Steffen Deusch <steffen@deusch.me>
2024-02-10 23:09:04 +0100

    use mix test

--- test/e2e/teardown.js
+++ test/e2e/teardown.js
@@ -1,6 +1,13 @@
 const request = require("@playwright/test").request;
 
 module.exports = async () => {
-  const context = await request.newContext({ baseURL: "http://localhost:4000" });
-  await context.post("/export-coverage")
+  try {
+    const context = await request.newContext({ baseURL: "http://localhost:4000" });
+    // gracefully stops the e2e script to export coverage
+    await context.post("/halt")
+  } catch (e) {
+    // we expect the request to fail because the request
+    // actually stops the server
+    return
+  }
 };
