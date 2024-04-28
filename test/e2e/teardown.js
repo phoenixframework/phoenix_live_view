5907a7a4c1a60ed1e3e081b1c4ab490cd5dfcd97
Steffen Deusch <steffen@deusch.me>
2024-04-28 12:34:14 +0200

    fix port in playwright teardown script

--- test/e2e/teardown.js
+++ test/e2e/teardown.js
@@ -2,7 +2,7 @@ const request = require("@playwright/test").request;
 
 module.exports = async () => {
   try {
-    const context = await request.newContext({ baseURL: "http://localhost:4000" });
+    const context = await request.newContext({ baseURL: "http://localhost:4004" });
     // gracefully stops the e2e script to export coverage
     await context.post("/halt")
   } catch (e) {
