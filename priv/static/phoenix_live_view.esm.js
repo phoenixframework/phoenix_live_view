b1187f0ff13fd12f54dab0aba3e4c5e228075742
Chris McCord <chris@chrismccord.com>
2024-07-19 09:15:42 -0400

    Bump morphdom to latest

--- priv/static/phoenix_live_view.esm.js
+++ priv/static/phoenix_live_view.esm.js
@@ -1951,6 +1951,7 @@ function morphdomFactory(morphAttrs2) {
           return;
         } else if (beforeUpdateResult instanceof HTMLElement) {
           fromEl = beforeUpdateResult;
+          indexTree(fromEl);
         }
         morphAttrs2(fromEl, toEl);
         onElUpdated(fromEl);
@@ -2228,7 +2229,7 @@ var DOMPatch = class {
           dom_default.maybeAddPrivateHooks(el, phxViewportTop, phxViewportBottom);
           this.trackBefore("added", el);
           let morphedEl = el;
-          if (!isJoinPatch && this.streamComponentRestore[el.id]) {
+          if (this.streamComponentRestore[el.id]) {
             morphedEl = this.streamComponentRestore[el.id];
             delete this.streamComponentRestore[el.id];
             morph.call(this, morphedEl, el, true);
@@ -3146,6 +3147,9 @@ var View = class {
     this.flash = null;
     if (this.root === this) {
       this.formsForRecovery = this.getFormsForRecovery();
+    }
+    if (this.isMain()) {
+      this.liveSocket.replaceRootHistory();
     }
     if (liveview_version !== this.liveSocket.version()) {
       console.error(`LiveView asset version mismatch. JavaScript version ${this.liveSocket.version()} vs. server ${liveview_version}. To avoid issues, please ensure that your assets use the same version as the server.`);
@@ -4900,6 +4904,9 @@ var LiveSocket = class {
     });
     for (let type of ["change", "input"]) {
       this.on(type, (e) => {
+        if (e instanceof CustomEvent && e.target.form === void 0) {
+          throw new Error(`dispatching a custom ${type} event is only supported on input elements inside a form`);
+        }
         let phxChange = this.binding("change");
         let input = e.target;
         if (e.isComposing) {
