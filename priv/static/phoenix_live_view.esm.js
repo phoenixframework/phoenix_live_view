0c29422eae26b86e5c7a095918ffd3807a52e351
Chris Nelson <superchrisnelson@gmail.com>
2024-09-19 14:46:11 -0400

    refactorin

--- priv/static/phoenix_live_view.esm.js
+++ priv/static/phoenix_live_view.esm.js
@@ -1547,50 +1547,38 @@ Hooks.InfiniteScroll = {
     };
   }
 };
+var serializeEvent = (event) => {
+  const { detail, target: { dataset } } = event;
+  return { ...detail, ...dataset };
+};
 Hooks.CustomEvents = {
   listeners: [],
-  serializeEvent(event) {
-    const { detail, target: { dataset } } = event;
-    return { ...detail, ...dataset };
+  pushCustomEvent(eventName, phxEvent) {
+    const attrs = this.el.attributes;
+    const phxTarget = attrs["phx-target"] && attrs["phx-target"].value;
+    const pushEvent = phxTarget ? (event, payload, callback) => this.pushEventTo(phxTarget, event, payload, callback) : (event, payload, callback) => this.pushEvent(event, payload, callback);
+    const listener = (evt) => {
+      const payload = serializeEvent(evt);
+      this.el.dispatchEvent(new CustomEvent("phx-event-start", { detail: { name: eventName, payload } }));
+      pushEvent(phxEvent, payload, (e) => {
+        this.el.dispatchEvent(new CustomEvent("phx-event-complete", { detail: { name: eventName, payload } }));
+      });
+    };
+    this.el.addEventListener(eventName, listener);
+    this.listeners.push({ eventName, listener });
   },
   mounted() {
-    const sendEvent = (eventName, phxEvent) => {
-      const attrs2 = this.el.attributes;
-      const phxTarget = attrs2["phx-target"] && attrs2["phx-target"].value;
-      const pushEvent = phxTarget ? (event, payload, callback) => this.pushEventTo(phxTarget, event, payload, callback) : (event, payload, callback) => this.pushEvent(event, payload, callback);
-      const listener = (evt) => {
-        const payload = this.serializeEvent(evt);
-        this.el.dispatchEvent(new CustomEvent("phx-event-start", { detail: { name: eventName, payload } }));
-        pushEvent(phxEvent, payload, (e) => {
-          this.el.dispatchEvent(new CustomEvent("phx-event-complete", { detail: { name: eventName, payload } }));
-        });
-      };
-      this.el.addEventListener(eventName, listener);
-      this.listeners.push({ eventName, listener });
-    };
     const attrs = this.el.attributes;
     for (var i = 0; i < attrs.length; i++) {
       if (/^phx-custom-event-/.test(attrs[i].name)) {
         const eventName = attrs[i].name.replace("phx-custom-event-", "");
         const phxEvent = attrs[i].value;
-        sendEvent(eventName, phxEvent);
+        this.pushCustomEvent(eventName, phxEvent);
       }
-    }
-    if (this.el.getAttribute("phx-send-events")) {
-      const eventsToSend = this.el.getAttribute("phx-send-events").split(",");
-      eventsToSend.forEach((eventName) => sendEvent(eventName, eventName));
     }
     if (this.el.getAttribute("phx-custom-events")) {
       const eventsToSend = this.el.getAttribute("phx-custom-events").split(",");
-      eventsToSend.forEach((eventName) => sendEvent(eventName, eventName));
-    }
-    if (this.el.getAttribute("phx-receive-events")) {
-      const phoenixEvents = this.el.getAttribute("phx-receive-events").split(",");
-      phoenixEvents.forEach((evt) => {
-        this.handleEvent(evt, (payload) => {
-          this.el.dispatchEvent(new CustomEvent(evt, { detail: payload }));
-        });
-      });
+      eventsToSend.forEach((eventName) => this.pushCustomEvent(eventName, eventName));
     }
   },
   destroyed() {
