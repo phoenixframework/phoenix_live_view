e3f06435ee6d25168c22ede031c19c5dad2e784c
Kelvin Stinghen <kelvin.stinghen@me.com>
2024-11-29 10:05:35 -0300

    Add assert_* functions to locals_without_parens config
    So one can simply add `:phoenix_live_view` to their `:plugins` option in `.formatter.exs` and it will not automatically include parens to those functions when `mix format`ing

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,13 @@
 [
   import_deps: [:phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    assert_patch: :*,
+    assert_patched: :*,
+    assert_push_event: :*,
+    assert_redirect: :*,
+    assert_redirected: :*,
+    assert_reply: :*
+  }
 ]
