6c6a91523507b06e9aa07dd8ea77a3a99f79d625
Steffen Deusch <steffen@deusch.me>
2024-12-20 13:47:05 +0100

    update dependencies, fix logger backends deprecation

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,6 @@
 import Config
 
 config :logger, :level, :debug
-config :logger, :backends, []
+config :logger, :default_handler, false
 
 config :phoenix_live_view, enable_expensive_runtime_checks: true
