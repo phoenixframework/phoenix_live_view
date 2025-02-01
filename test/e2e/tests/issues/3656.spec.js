5ba767c6d861ff9d5e69c6160ed707d3ccd3ab0a
Steffen Deusch <steffen@deusch.me>
2025-02-01 17:33:12 +0100

    fix phx-remove and phx-click-loading in sticky LVs
    Fixes #3656
    Fixes #3658

---
+++ test/e2e/tests/issues/3656.spec.js
@@ -0,0 +1,21 @@
+const {test, expect} = require("../../test-fixtures")
+const {syncLV, attributeMutations} = require("../../utils")
+
+// https://github.com/phoenixframework/phoenix_live_view/issues/3656
+test("phx-click-loading is removed from links in sticky LiveViews", async ({page}) => {
+  await page.goto("/issues/3656")
+  await syncLV(page)
+
+  let changes = attributeMutations(page, "nav a")
+
+  const link = page.getByRole("link", {name: "Link 1"})
+  await link.click()
+
+  await syncLV(page)
+  await expect(link).not.toHaveClass("phx-click-loading")
+
+  expect(await changes()).toEqual(expect.arrayContaining([
+    {attr: "class", oldValue: null, newValue: "phx-click-loading"},
+    {attr: "class", oldValue: "phx-click-loading", newValue: ""},
+  ]))
+})
