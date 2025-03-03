c44c48e09c3705fd9510d491d77f5237929be08e
Kennedy <5649630+thoughtarray@users.noreply.github.com>
2025-03-03 09:36:43 -0600

    JS.transition example html tag fix (#3698)

--- lib/phoenix_live_view/js.ex
+++ lib/phoenix_live_view/js.ex
@@ -674,7 +674,7 @@ defmodule Phoenix.LiveView.JS do
 
   <div phx-mounted={JS.transition({"ease-out duration-300", "opacity-0", "opacity-100"}, time: 300)}>
       duration-300 milliseconds matches time: 300 milliseconds
-  <div>
+  </div>
   ```
   """
   def transition(transition) when is_binary(transition) or is_tuple(transition) do
