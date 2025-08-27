a3756ac63b2093b065cfd1418bdd6670babd7fe4
Boris Kuznetsov <me@achempion.com>
2025-08-27 10:32:55 +0200

    Resolve moved comprehensions in test client proxy (#3963)

--- test/phoenix_live_view/test/diff_test.exs
+++ test/phoenix_live_view/test/diff_test.exs
@@ -11,5 +11,35 @@ defmodule Phoenix.LiveViewTest.DiffTest do
       assert Diff.merge_diff(%{s: "foo", d: []}, %{s: "bar"}) ==
                %{s: "bar", streams: []}
     end
+
+    test "resolves moved comprehensions" do
+      base = %{
+        k: %{
+          0 => %{0 => "A"},
+          1 => %{0 => "B"},
+          2 => %{0 => "C", 1 => %{0 => "var1", :s => ["", ""]}},
+          :kc => 3
+        }
+      }
+
+      diff = %{
+        k: %{
+          0 => 1,
+          1 => [2, %{1 => %{0 => "var2"}}],
+          :kc => 2
+        }
+      }
+
+      result = %{
+        k: %{
+          0 => %{0 => "B"},
+          1 => %{0 => "C", 1 => %{0 => "var2", :s => ["", ""]}},
+          :kc => 2
+        },
+        streams: []
+      }
+
+      assert Diff.merge_diff(base, diff) == result
+    end
   end
 end
