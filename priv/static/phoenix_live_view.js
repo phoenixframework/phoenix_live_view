f5ee3f88b91cd51f0711c07e207ec992cc64b3f3
SteffenDE <4116351+SteffenDE@users.noreply.github.com>
2025-10-26 09:17:05 +0000

    Update assets

--- priv/static/phoenix_live_view.js
+++ priv/static/phoenix_live_view.js
@@ -5920,7 +5920,7 @@ removing illegal node: "${(childNode.outerHTML || childNode.nodeValue).trim()}"
       this.owner(el, (view) => js_default.exec(e, eventType, encodedJS, view, el));
     }
     /**
-     * Returns an object with methods to manipluate the DOM and execute JavaScript.
+     * Returns an object with methods to manipulate the DOM and execute JavaScript.
      * The applied changes integrate with server DOM patching.
      *
      * @returns {import("./js_commands").LiveSocketJSCommands}
