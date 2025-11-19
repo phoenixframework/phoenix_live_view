ea1a439e9720e4a0a42d6aa281911bcb2f4ef0af
Coby Benveniste <41164052+probably-not@users.noreply.github.com>
2025-11-19 20:27:43 +0200

    Allow Styling Phoenix Uploads On Drag And Drop (#4012)
    * Listen for dragenter and dragleave events and add phx-files-dropzone to the closest drop zone
    
    * Add PHX_DROPZONE_CLASS constant
    
    * Update the documentation to include phx-drop-target-active docs
    
    * Use a one-off dragleave listener on elements that have the dropzone class added by dragging.
    
    Co-authored-by: Steffen Deusch <steffen@deusch.me>
    
    * Rename constant
    
    Co-authored-by: Steffen Deusch <steffen@deusch.me>
    
    * Fix missed rename in docs
    
    Co-authored-by: Steffen Deusch <steffen@deusch.me>
    
    * Link component docs to the uploads guide
    
    Co-authored-by: Steffen Deusch <steffen@deusch.me>
    
    * Use this.js() instead of classList.add to avoid patch removing changes
    
    Co-authored-by: Steffen Deusch <steffen@deusch.me>
    
    * Fixes based on the suggestions
    
    * Fix heredoc indent
    
    * Revert "Use a one-off dragleave listener on elements that have the dropzone class added by dragging."
    
    This reverts commit 8728589fae5fb612ce5bb39c042d76c30f6ed4d6.
    
    ---------
    
    Co-authored-by: Steffen Deusch <steffen@deusch.me>

--- assets/js/phoenix_live_view/live_socket.js
+++ assets/js/phoenix_live_view/live_socket.js
@@ -28,6 +28,7 @@ import {
   PHX_REF_SRC,
   PHX_RELOAD_STATUS,
   PHX_RUNTIME_HOOK,
+  PHX_DROP_TARGET_ACTIVE_CLASS,
 } from "./constants";
 
 import {
@@ -37,6 +38,7 @@ import {
   debug,
   maybe,
   logError,
+  eventContainsFiles,
 } from "./utils";
 
 import Browser from "./browser";
@@ -666,14 +668,55 @@ export default class LiveSocket {
       },
     );
     this.on("dragover", (e) => e.preventDefault());
+    this.on("dragenter", (e) => {
+      const dropzone = closestPhxBinding(
+        e.target,
+        this.binding(PHX_DROP_TARGET),
+      );
+
+      if (!dropzone || !(dropzone instanceof HTMLElement)) {
+        return;
+      }
+
+      if (eventContainsFiles(e)) {
+        this.js().addClass(dropzone, PHX_DROP_TARGET_ACTIVE_CLASS);
+      }
+    });
+    this.on("dragleave", (e) => {
+      const dropzone = closestPhxBinding(
+        e.target,
+        this.binding(PHX_DROP_TARGET),
+      );
+
+      if (!dropzone || !(dropzone instanceof HTMLElement)) {
+        return;
+      }
+
+      // Avoid add/remove jitter in the case that we drag into a new child and that child would
+      // resolve their closest drop target to the current dropzone element
+      const rect = dropzone.getBoundingClientRect();
+      if (
+        e.clientX <= rect.left ||
+        e.clientX >= rect.right ||
+        e.clientY <= rect.top ||
+        e.clientY >= rect.bottom
+      ) {
+        this.js().removeClass(dropzone, PHX_DROP_TARGET_ACTIVE_CLASS);
+      }
+    });
     this.on("drop", (e) => {
       e.preventDefault();
-      const dropTargetId = maybe(
-        closestPhxBinding(e.target, this.binding(PHX_DROP_TARGET)),
-        (trueTarget) => {
-          return trueTarget.getAttribute(this.binding(PHX_DROP_TARGET));
-        },
+
+      const dropzone = closestPhxBinding(
+        e.target,
+        this.binding(PHX_DROP_TARGET),
       );
+      if (!dropzone || !(dropzone instanceof HTMLElement)) {
+        return;
+      }
+      this.js().removeClass(dropzone, PHX_DROP_TARGET_ACTIVE_CLASS);
+
+      const dropTargetId = dropzone.getAttribute(this.binding(PHX_DROP_TARGET));
       const dropTarget = dropTargetId && document.getElementById(dropTargetId);
       const files = Array.from(e.dataTransfer.files || []);
       if (
