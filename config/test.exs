1955216f535a6e2d3d8c8db65d3390170f4161eb
Steffen Deusch <steffen@deusch.me>
2026-01-24 17:17:02 +0100

    Update config/test.exs
    Co-authored-by: David Green <134172184+green-david@users.noreply.github.com>

--- config/test.exs
+++ config/test.exs
@@ -4,8 +4,3 @@ config :logger, :level, :debug
 config :logger, :default_handler, false
 
 config :phoenix_live_view, enable_expensive_runtime_checks: true
-
-# Disable :root_tag_attribute so the majority of the tests
-# are not polluted by it. It will be explicitly re-enabled for
-# tests related to :root_tag_attribute functionality.
-config :phoenix_live_view, :root_tag_attribute, nil
