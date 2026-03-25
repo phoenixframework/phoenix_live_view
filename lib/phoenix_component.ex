a6408e57d5ee33629a287c7a7209f0bc153eb6a2
Barna Kovacs <barna.kovacs@tutanota.com>
2026-03-25 11:59:04 +0100

    Fix docs: <.link> component does not generate a form for non-get requests (#4179)

--- lib/phoenix_component.ex
+++ lib/phoenix_component.ex
@@ -3063,8 +3063,10 @@ defmodule Phoenix.Component do
     and therefore only works with the `href={...}` attribute. It has no effect on `patch`
     and `navigate` instructions.
 
-    In case the method is not `get`, the link is generated inside the form which sets the proper
-    information. In order to submit the form, JavaScript must be enabled in the browser.
+    In case the method is not `get`, the link is generated with the `data-method`
+    attribute that is intercepted by the `phoenix_html` JavaScript library to
+    trigger a form submission. In order to submit the form, JavaScript must
+    be enabled in the browser.
     """
   )
 
