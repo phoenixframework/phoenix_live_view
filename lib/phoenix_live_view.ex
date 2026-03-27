f7684682ca82d35483dc827b4ce4a3e1638707af
Barna Kovacs <barna.kovacs@tutanota.com>
2026-03-27 11:09:50 +0100

    Fix docs: allow_upload :writer expects a 3-arity anonymous function, not a module (#4181)

--- lib/phoenix_live_view.ex
+++ lib/phoenix_live_view.ex
@@ -846,10 +846,10 @@ defmodule Phoenix.LiveView do
     * `:auto_upload` - Instructs the client to upload the file automatically
       on file selection instead of waiting for form submits. Defaults to `false`.
 
-    * `:writer` - A module implementing the `Phoenix.LiveView.UploadWriter`
-      behaviour to use for writing the uploaded chunks. Defaults to writing to a
-      temporary file for consumption. See the `Phoenix.LiveView.UploadWriter` docs
-      for custom usage.
+    * `:writer` - A 3-arity anonymous function that returns a tuple with a module
+      implementing the `Phoenix.LiveView.UploadWriter` behaviour and its options to use for
+      writing the uploaded chunks. Defaults to writing to a temporary file for consumption.
+      See the `Phoenix.LiveView.UploadWriter` docs for custom usage.
 
   Raises when a previously allowed upload under the same name is still active.
 
