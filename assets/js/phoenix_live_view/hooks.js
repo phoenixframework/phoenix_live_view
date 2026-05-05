7a8b43afc3f7807223d11690554f2f9cb1410bd3
Steffen Deusch <steffen@deusch.me>
2026-05-05 12:31:47 +0200

    Ensure phx-viewport hook does not fail if there's no scrollContainer
    Closes #4214.

--- assets/js/phoenix_live_view/hooks.js
+++ assets/js/phoenix_live_view/hooks.js
@@ -262,7 +262,7 @@ Hooks.InfiniteScroll = {
   updated() {
     // Check if the scroll container still exists
     // https://github.com/phoenixframework/phoenix_live_view/issues/4169.
-    if (!this.scrollContainer.isConnected) {
+    if (this.scrollContainer && !this.scrollContainer.isConnected) {
       this.destroyed();
       this.mounted();
     }
