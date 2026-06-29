d4b8703bf8dfe03dcdf6c9b34b60cb82842cfbf9
Frank Polasek Dugan III <frank3@duganator.com>
2026-06-29 02:50:23 -0500

    fix: don't drop directives with multiple macro components (#4319)
    * fix: don't drop directives with multiple macro components
    
    * format test

--- test/phoenix_component/macro_component_integration_test.exs
+++ test/phoenix_component/macro_component_integration_test.exs
@@ -53,6 +53,15 @@ defmodule Phoenix.Component.MacroComponentIntegrationTest do
     end
   end
 
+  defmodule NoDirectiveMacroComponent do
+    @behaviour Phoenix.Component.MacroComponent
+
+    @impl true
+    def transform(_ast, _meta) do
+      {:ok, ""}
+    end
+  end
+
   test "receives ast" do
     defmodule TestComponentAst do
       use Phoenix.Component
@@ -468,6 +477,28 @@ defmodule Phoenix.Component.MacroComponentIntegrationTest do
 
                  <p phx-r>Part of the component</p>
                </div>
+               """
+    end
+
+    test "are not dropped by a later macro component that emits no directives" do
+      defmodule TestComponentDirectivesPreserved do
+        use Phoenix.Component
+
+        def render(assigns) do
+          ~H"""
+          <div :type={DirectiveMacroComponent}></div>
+          <div id="hello">
+            <script :type={NoDirectiveMacroComponent}>
+            </script>
+          </div>
+          """
+        end
+      end
+
+      assert render_component(&TestComponentDirectivesPreserved.render/1)
+             |> TreeDOM.normalize_to_tree(sort_attributes: true) ==
+               ~X"""
+               <div id="hello" phx-sample-one="test" phx-sample-two="test" phx-r></div>
                """
     end
 
