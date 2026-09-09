afa21cc28b2543001bb25db4f24e5ff65c58b461
Lukasz Samson <lukaszsamson@gmail.com>
2026-09-09 15:48:36 +0200

    Preserve caret for additional input types

--- assets/js/phoenix_live_view/dom.ts
+++ assets/js/phoenix_live_view/dom.ts
@@ -649,7 +649,10 @@ const DOM = {
 
   hasSelectionRange(el): el is HTMLInputElement | HTMLTextAreaElement {
     return (
-      el.setSelectionRange && (el.type === "text" || el.type === "textarea")
+      el.setSelectionRange &&
+      ["text", "textarea", "search", "url", "tel", "password"].includes(
+        el.type,
+      )
     );
   },
 
